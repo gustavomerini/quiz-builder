bd4725556bf1389e0d1d3bb63ca9a390b1793d3c
merini <gutomerini@gmail.com>
2019-05-17 00:48:23 -0300

    Initial Commit
    Basic quiz builder setup on App.js with static state ready to code split into components.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+export class App extends Component {
+  state = {
+      title: "My Quiz Title",
+      questions: []
+  };
+  
+  createQuestionHandler = () => {
+    const updatedQuestions = [...this.state.questions];
+    updatedQuestions.push({ title: "", a: "", b: "", c: "" });
+    this.setState({ questions: updatedQuestions });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Quiz Builder!</h1>
+        </header>
+        <div className="App-quiz-builder">
+          <input placeholder="Quiz Title" />
+          {this.state.questions.map((question, i) => {
+            return (
+              <div className="App-questions" key={i}>
+                <input placeholder="Question Title" />
+                <input placeholder="A" />
+                <input placeholder="B" />
+                <input placeholder="C" />
+              </div>
+            );
+          })}
+          <button onClick={this.createQuestionHandler}>Add new question</button>
+        </div>
+      </div>
+    );
+  }
 }
+
+/* 
+
+          {this.state.questions.map(question => {
+            return (
+              <div>
+                <h2> {question.title} </h2>
+                <h4> {question.a} </h4>
+                <h4> {question.b} </h4>
+                <h4> {question.c} </h4>
+              </div>
+            );
+          })}
+
+*/
 
 export default App;
