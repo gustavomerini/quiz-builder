5df60aaaee8ee612a4ee773b176d4bea48ac9558
merini <gutomerini@gmail.com>
2019-05-18 12:01:55 -0300

    Few changes on the structure.
    formik dep added.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import React, { Component } from "react";
 
 export class App extends Component {
   state = {
-      title: "My Quiz Title",
-      questions: []
+    title: "My Quiz Title",
+    questions: []
   };
-  
+
   createQuestionHandler = () => {
     const updatedQuestions = [...this.state.questions];
     updatedQuestions.push({ title: "", a: "", b: "", c: "" });
     this.setState({ questions: updatedQuestions });
-    console.log(this.state);
-  }
+  };
+
+  saveQuizHandler = e => {
+    e.preventDefault();
+    console.log(e.target);
+  };
 
   render() {
     return (
@@ -21,20 +25,23 @@ export class App extends Component {
         <header className="App-header">
           <h1>Quiz Builder!</h1>
         </header>
-        <div className="App-quiz-builder">
+        <form className="App-quiz-builder" onSubmit={this.saveQuizHandler} onChange={this.handleChange}  >
           <input placeholder="Quiz Title" />
-          {this.state.questions.map((question, i) => {
+          {this.state.questions.map((_, i) => {
             return (
               <div className="App-questions" key={i}>
-                <input placeholder="Question Title" />
-                <input placeholder="A" />
-                <input placeholder="B" />
-                <input placeholder="C" />
+                <input id="questionTitle" placeholder="Question Title" />
+                <input id="a" placeholder="A" />
+                <input id="b" placeholder="B" />
+                <input id="c" placeholder="C" />
               </div>
             );
           })}
-          <button onClick={this.createQuestionHandler}>Add new question</button>
-        </div>
+          <button type="button" onClick={this.createQuestionHandler}>
+            Add new question
+          </button>
+          <button type="submit">Save Quiz</button>
+        </form>
       </div>
     );
   }
