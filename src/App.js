0b85d6ace92c01cb1769a44e97d0536ed2baa53a
gustavomerini <gutomerini@gmail.com>
2019-05-18 14:01:17 -0300

    Simple formik setup
    Simple setup to use formik, basic app structure to create/remove a question

--- src/App.js
+++ src/App.js
@@ -1,65 +1,142 @@
 import "./App.css";
 
+import * as yup from "yup";
 import React, { Component } from "react";
+import { Formik, Form, Field, FieldArray, ErrorMessage } from "formik";
+
+const initialState = {
+    title: "",
+    questions: [
+        {
+            title: "",
+            a: "",
+            b: "",
+            c: "",
+        },
+    ],
+};
+
+const signupValidationSchema = yup.object().shape({
+    title: yup.string().required("Quiz Title is required"),
+    questions: yup.array().of(
+        yup.object().shape({
+            title: yup.string().required("Question Title is required"),
+            a: yup.string().required("A is required"),
+        })
+    ),
+});
 
 export class App extends Component {
-  state = {
-    title: "My Quiz Title",
-    questions: []
-  };
+    state = {
+        title: "",
+        questions: [],
+    };
 
-  createQuestionHandler = () => {
-    const updatedQuestions = [...this.state.questions];
-    updatedQuestions.push({ title: "", a: "", b: "", c: "" });
-    this.setState({ questions: updatedQuestions });
-  };
+    createQuestionHandler = () => {
+        const updatedQuestions = [...this.state.questions];
+        updatedQuestions.push({ title: "", a: "", b: "", c: "" });
+        this.setState({ questions: updatedQuestions });
+    };
 
-  saveQuizHandler = e => {
-    e.preventDefault();
-    console.log(e.target);
-  };
+    saveQuizHandler = (values, actions) => {
+        this.setState({ ...values });
+        actions.setSubmitting(false);
+    };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Quiz Builder!</h1>
-        </header>
-        <form className="App-quiz-builder" onSubmit={this.saveQuizHandler} onChange={this.handleChange}  >
-          <input placeholder="Quiz Title" />
-          {this.state.questions.map((_, i) => {
-            return (
-              <div className="App-questions" key={i}>
-                <input id="questionTitle" placeholder="Question Title" />
-                <input id="a" placeholder="A" />
-                <input id="b" placeholder="B" />
-                <input id="c" placeholder="C" />
-              </div>
-            );
-          })}
-          <button type="button" onClick={this.createQuestionHandler}>
-            Add new question
-          </button>
-          <button type="submit">Save Quiz</button>
-        </form>
-      </div>
-    );
-  }
+    // #TODO code-splitting
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Quiz Builder!</h1>
+                </header>
+                <Formik
+                    validationSchema={signupValidationSchema}
+                    initialValues={initialState}
+                    onSubmit={this.saveQuizHandler}
+                    render={({ values, isSubmitting }) => (
+                        <Form>
+                            <Field name="title" type="text" />
+                            <ErrorMessage name="title">
+                                {msg => (
+                                    <div className="error error-message">
+                                        {msg}
+                                    </div>
+                                )}
+                            </ErrorMessage>
+                            <FieldArray
+                                name="questions"
+                                render={arrayHelpers => (
+                                    <div className="App-quiz-builder">
+                                        {values.questions.map(
+                                            (question, index) => (
+                                                <div
+                                                    className="App-questions"
+                                                    key={index}
+                                                >
+                                                    <Field
+                                                        placeholder="Question Title"
+                                                        name={`questions[${index}].title`}
+                                                    />
+                                                    <ErrorMessage
+                                                        name={`questions[${index}].title`}
+                                                    >
+                                                        {msg => (
+                                                            <div className="error error-message">
+                                                                {msg}
+                                                            </div>
+                                                        )}
+                                                    </ErrorMessage>
+                                                    <Field
+                                                        name={`questions.${index}.a`}
+                                                    />
+                                                    <ErrorMessage
+                                                        name={`questions[${index}].a`}
+                                                    >
+                                                        {msg => (
+                                                            <div className="error error-message">
+                                                                {msg}
+                                                            </div>
+                                                        )}
+                                                    </ErrorMessage>
+                                                    <button
+                                                        type="button"
+                                                        onClick={() =>
+                                                            arrayHelpers.remove(
+                                                                index
+                                                            )
+                                                        }
+                                                    >
+                                                        Remove Question
+                                                    </button>
+                                                </div>
+                                            )
+                                        )}
+                                        <button
+                                            type="button"
+                                            onClick={() =>
+                                                arrayHelpers.push({
+                                                    title: "",
+                                                    a: "",
+                                                    b: "",
+                                                    c: "",
+                                                })
+                                            }
+                                        >
+                                            Add Question
+                                        </button>
+                                    </div>
+                                )}
+                            />
+                            <button type="submit" disabled={isSubmitting}>
+                                Save Quiz
+                            </button>
+                        </Form>
+                    )}
+                />
+            </div>
+        );
+    }
 }
-
-/* 
-
-          {this.state.questions.map(question => {
-            return (
-              <div>
-                <h2> {question.title} </h2>
-                <h4> {question.a} </h4>
-                <h4> {question.b} </h4>
-                <h4> {question.c} </h4>
-              </div>
-            );
-          })}
-
-*/
 
 export default App;
